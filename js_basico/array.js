ed623e6585fd6525502fed067bbedb34189ab40d
JoseSamueldeMeloSantos <josesamuelsantos321@hotmail.com>
2025-10-13 21:32:12 -0300

    adicionando oq faltava

---
+++ js_basico/array.js
@@ -0,0 +1,29 @@
+//declaração de array
+const alunos = ['luiz', 'maria', 'joao']
+
+//acesso de alunos
+alunos[0]
+
+//alterando um dado
+alunos[0] = 'eduardor'
+
+//para retornar o tamanho do array
+alunos.length
+
+//para adicionar no fim da lista
+alunos.push('clara')
+
+//adiciona no começo da lista
+alunos.unshift("José")
+
+//remove o ultimo aluno da lista
+alunos.pop()
+
+//remove o primeiro aluno da lista
+alunos.shift()
+
+//para fatiar uma lista
+alunos.slice(0,3)
+
+//verificar instancia
+alunos instanceof Array
