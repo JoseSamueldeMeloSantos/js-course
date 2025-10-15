9b7e2bbb7f2602e47efca4036aa59b15da458dac
JoseSamueldeMeloSantos <josesamuelsantos321@hotmail.com>
2025-10-15 06:39:29 -0300

    logic operators + short-circuit

--- logica-prog/operadores.js
+++ logica-prog/operadores.js
@@ -1,15 +1,20 @@
 /**
- * OPERADORES LOGICOS
+ * OPERADORES de comparação
  
     >
     >=
     <
     <=
-    ==
-    ===
-    != 
-    !==
-    
-  
- */
+    == igualdade (vai checar apenas o valor) ***não vamos usar este***
+    === igualdade estrita(vai checar valor e tipo)
+    != diferente (vai checar apenas o valor) ***não vamos usar este***
+    !== diferente estrito (vai checar valor e tipo)
+
+    Operadores logicos
+
+    &&  and
+    ||  or
+    !   not
+
+*/
   
