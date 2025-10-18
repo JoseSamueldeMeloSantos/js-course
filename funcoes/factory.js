40a62acb0e442ac8f7d5b1efdc03d09e5f93fda4
JoseSamueldeMeloSantos <josesamuelsantos321@hotmail.com>
2025-10-18 14:43:05 -0300

    factory

---
+++ funcoes/factory.js
@@ -0,0 +1,7 @@
+/**
+ * Uma função factory (ou factory function)
+ *  é uma função que cria e retorna objetos — ou seja, uma “fábrica de objetos”.
+ * 
+ * Em vez de usar class ou new, você usa uma função comum que monta e devolve um objeto pronto pra uso.
+ */
+
--- funcoes/funcao-imediata.js
+++ funcoes/funcao-imediata.js
@@ -3,3 +3,10 @@
 (function(num1, num2) {
     console.log(num1 + num2)
 })(1,3)
+
+/**
+ * 
+(function() { ... }) → cria uma função anônima (sem nome).
+Os parênteses externos () transformam ela em uma expressão (Expression).
+O segundo par de parênteses () no final executa a função na hora.
+ */
