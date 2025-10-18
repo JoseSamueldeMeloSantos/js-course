e46396b912dd309022f54f99a5d268531f26a77c
JoseSamueldeMeloSantos <josesamuelsantos321@hotmail.com>
2025-10-18 16:59:59 -0300

    filter

---
+++ array/filter.js
@@ -0,0 +1,24 @@
+/**
+O filter é um método retorna um array filtrado 
+a partir de uma função callback recebida 
+
+obs
+    -a função callback deve sempre retornar true ou false
+    
+    -a função callback sempre poder ter acesso  ao valor,indice e array da função que irá filtrar
+
+    -o filter sempre vai retornar um array
+
+    estrutura
+        a.filter((valor, indice, array) => {
+        })
+ */
+
+const nums = [1,2,3,4,5,6,7,8,9];
+
+let numerosPares = nums.filter((valor) => {
+    return (valor % 2) == 0;
+});
+
+console.log(numerosPares);
+
