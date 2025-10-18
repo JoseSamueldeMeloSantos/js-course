ef1c512e44f233dc5102523b621f9e8fbe2065ea
JoseSamueldeMeloSantos <josesamuelsantos321@hotmail.com>
2025-10-18 14:43:19 -0300

    factory

--- funcoes/factory.js
+++ funcoes/factory.js
@@ -5,3 +5,20 @@
  * Em vez de usar class ou new, você usa uma função comum que monta e devolve um objeto pronto pra uso.
  */
 
+function criarPessoa(nome, idade) {
+  return {
+    nome,
+    idade,
+
+    //metodo
+    apresentar() {
+      console.log(`Olá! Meu nome é ${this.nome} e tenho ${this.idade} anos.`);
+    }
+  };
+}
+
+const pessoa1 = criarPessoa("Samuel", 20);
+const pessoa2 = criarPessoa("Ana", 22);
+
+pessoa1.apresentar();
+pessoa2.apresentar();
